8c4e26a0c93f1a2d643e1265c3ef85eb3086ff9e
bibiu020 <118839619+bibiu020@users.noreply.github.com>
2022-12-07 11:48:33 -0300

    dia7/12

---
+++ arquivo.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var url = requiree ('url');
+var fs = require('fs');
+http.createServer(function(req,res) {
+var q = url.parse(req.url,true).query;
+res.write('arquivo criado com sucesso!');
+fs.appendFile(q.nome+'.txt', q.conteudo, function (err) {
+    if (err) throw err; 
+});
+res.end();
+}).listen(80);
